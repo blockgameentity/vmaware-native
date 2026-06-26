f466699f0d738361cccc031b2ef0a5fee46636d7
desu <108037609+blockgameentity@users.noreply.github.com>
2026-06-26 04:34:52 +0200

    updated build script

--- build.rs
+++ build.rs
@@ -2,39 +2,16 @@ use std::path::Path;
 
 fn main() {
     let root = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let vmaware_hpp = Path::new(&root).join("vmaware.hpp");
+    let vendor = Path::new(&root).join("vendor");
 
-    if !vmaware_hpp.exists() {
-        const URL: &str =
-            "https://raw.githubusercontent.com/kernelwernel/VMAware/a4c25ff8854e3833d1fc8491ad8692193b7b7d6c/src/vmaware.hpp";
-        eprintln!("cargo:warning=vmaware.hpp not found — downloading from {URL}");
-
-        let status = std::process::Command::new("curl")
-            .args([
-                "--fail",
-                "--silent",
-                "--show-error",
-                "--location",
-                "-o",
-                vmaware_hpp.to_str().unwrap(),
-                URL,
-            ])
-            .status()
-            .expect("failed to invoke `curl`");
-
-        assert!(
-            status.success(),
-            "curl exited with {status} while downloading vmaware.hpp from {URL}"
-        );
-    }
-
-    println!("cargo:rerun-if-changed=vmaware.hpp");
+    println!("cargo:rerun-if-changed=vendor/vmaware.hpp");
 
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
 
     cxx_build::bridge("src/ffi.rs")
         .file("src/bridge.cpp")
-        .include(&root)
+        .include(&vendor)
+        .include(".")
         .flag_if_supported("/std:c++20")
         .flag_if_supported("-std=c++20")
         .compile("vmaware_bridge");
